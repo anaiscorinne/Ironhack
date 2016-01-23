6a4c05974ec994686466cf85cfbb35f6f67e8cff
Anais Engelmajer <anaiscorinne@gmail.com>
2016-01-23 00:02:01 -0500

    FInished 2nd and 3rd iterations including randomizer and sort

--- week2/exercises/imdb/app.rb
+++ week2/exercises/imdb/app.rb
@@ -2,16 +2,40 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'imdb'
 
+require_relative('lib/poster.rb')
+
+movie = Movie.new
+
 
 get "/" do
 	erb(:home)
 end
 
 get "/imdb_test" do
-	search = Imdb::Search.new("")
-
-	@number_of_results = search.movies.length
-	@first_result = search.movies[]
+	@search_result = params[:search]
+	@movie_posters = movie.movie_search(@search_result)
+	@number_of_results = @movie_posters.length
+	movie.randomizer
+	@random_movie = movie.random_movie
+	# @random_movie = random_movie
 
 	erb(:imdb_test)
 end
+
+post "/quiz_answer" do
+	@quiz_title = params[:movie_title]
+	@random_movie_title = params[:random_movie]
+
+	if @quiz_title = @random_movie_title
+		redirect to "/answer"
+	else
+		erb(:wrong_answer)
+	end
+end
+
+get "/answer" do
+	erb(:answer)
+end
+
+
+
