aa463061029d632514f886f4c7920a4040e85a26
Anais Engelmajer <anaiscorinne@gmail.com>
2016-01-27 12:11:01 -0500

    Made code more concise for iteration 1

--- week3/exercises/decoder/decoder.js
+++ week3/exercises/decoder/decoder.js
@@ -1,30 +1,11 @@
-var words, message;
-
-words = [
-    "dead",         // 1st -> d
-    "bygone",       // 2nd -> y
-    "landing",      // 3rd -> n
-    "cheaply",      // 4th -> a
-    "assumed",      // 5th -> m
-    "incorrectly",  // 1st -> i
-    "attention",    // 2nd -> t
-    "agent"         // 3rd -> e
-];
-
-
-
 function decoder (arrayOfWords) {
-    var i = 0;
+    var index = 0;
     var charCounter = 0;
     var secretMessage = "";
-    while (i < arrayOfWords.length){
-        secretMessage += arrayOfWords[i].charAt(charCounter);
-        charCounter = charCounter + 1;
-        i++;
-        if (charCounter % 5 === 0){
-            charCounter = 0;
-        };
-    };
+    for (var i =0; i < words.length; i++) {
+        secretMessage =+ words[i].charAt(index);
+        index = (index + 1) % 5;
+    }
     return secretMessage
 }
 
