76e010a66d3a80a3280f7c4c8ee3990c23a8f068
Anais Engelmajer <anaiscorinne@gmail.com>
2016-01-27 12:27:40 -0500

    Separated files and used require

---
+++ week3/exercises/decoder/app.js
@@ -0,0 +1,5 @@
+var decoder = require('./decoder.js').decoder;
+
+var words = ['Mouse', 'Tiger', 'Class', 'Summer', 'Miami'];
+var result = decoder(words);
+console.log(result);
--- week3/exercises/decoder/decoder.js
+++ week3/exercises/decoder/decoder.js
@@ -1,14 +1,11 @@
-function decoder (arrayOfWords) {
+function decoder (words) {
     var index = 0;
-    var charCounter = 0;
     var secretMessage = "";
     for (var i =0; i < words.length; i++) {
-        secretMessage =+ words[i].charAt(index);
+        secretMessage += words[i].charAt(index);
         index = (index + 1) % 5;
     }
     return secretMessage
 }
 
-
-message = decoder(words);
-console.log(message);
+module.exports.decoder = decoder;
