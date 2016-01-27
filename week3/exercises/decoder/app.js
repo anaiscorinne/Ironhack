e509b908b2ceb40977be9f2a479ba81d7794c279
Anais Engelmajer <anaiscorinne@gmail.com>
2016-01-27 12:32:45 -0500

    Created lib and other multiple folders

--- week3/exercises/decoder/app.js
+++ week3/exercises/decoder/app.js
@@ -1,5 +1,7 @@
-var decoder = require('./decoder.js').decoder;
+var decoder = require('./lib/decoder.js').decoder;
+var sayHello = require('./lib/say_hello.js');
 
 var words = ['Mouse', 'Tiger', 'Class', 'Summer', 'Miami'];
 var result = decoder(words);
 console.log(result);
+sayHello();
