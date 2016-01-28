a51d6b5769ad528a411522b12432e35db8ab73fe
Anais Engelmajer <anaiscorinne@gmail.com>
2016-01-28 16:13:12 -0500

    Finished iteration 3 to sort

--- week3/exercises/GoT/app.js
+++ week3/exercises/GoT/app.js
@@ -3,19 +3,27 @@ function fileActions(err, file){
     if (err) {
         throw err;
     }
+  else{
     var episodes = JSON.parse(file);
-      episodePrinter(episodes)
+    sorted_array=sortedEpisodes(episodes);
+    episodePrinter (sorted_array);
 }
-  
+}
 function episodePrinter (episode_array) {
+   
     for (var i = 0; i < episode_array.length ; i++) {
-        
+       
       console.log("Title: " + episode_array[i].title + " Number: " + episode_array[i].episode_number );
       console.log(episode_array[i].description);
       console.log("Rating: " + episode_array[i].rating + starAdd(episode_array[i].rating))
     }
 }
 
+ function sortedEpisodes(episode_array) {
+  return episode_array.sort(function(a, b){
+        return a.episode_number-b.episode_number
+        });
+}
 
 function starAdd (number) {
    var stars = " "
@@ -25,4 +33,14 @@ function starAdd (number) {
     return stars
 }
 
+
+
 fs.readFile("./GoTEpisodes.json", 'utf8', fileActions);
+
+
+
+
+
+
+
+
