f4b814e3798fe32c0f9caf38bcc9ff36fd93cf86
Anais Engelmajer <anaiscorinne@gmail.com>
2016-01-28 15:56:27 -0500

    Game of Thrones exercise Iteration 2 complete

---
+++ week3/exercises/GoT/app.rb
@@ -0,0 +1,28 @@
+var fs = require('fs');
+function fileActions(err, file){ 
+    if (err) {
+        throw err;
+    }
+    var episodes = JSON.parse(file);
+      episodePrinter(episodes)
+}
+  
+function episodePrinter (episode_array) {
+    for (var i = 0; i < episode_array.length ; i++) {
+        
+      console.log("Title: " + episode_array[i].title + " Number: " + episode_array[i].episode_number );
+      console.log(episode_array[i].description);
+      console.log("Rating: " + episode_array[i].rating + starAdd(episode_array[i].rating))
+    }
+}
+
+
+function starAdd (number) {
+   var stars = " "
+   for (var rate = 0; rate < number; rate++) {
+       stars += "*"
+    }
+    return stars
+}
+
+fs.readFile("./GoTEpisodes.json", 'utf8', fileActions);
