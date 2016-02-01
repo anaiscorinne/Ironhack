43804224f3289394122b6d44c684788fff8299d9
Anais Engelmajer <anaiscorinne@gmail.com>
2016-02-01 18:29:17 -0500

    Initialized project and finished Iteration 1

---
+++ week4/exercises/address_book/db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# unless Contact.all.any?
+10.times do |i|
+	Contact.create(name: "Contact Name #{i}", address: "#{i} Miami Beach, FL 33140", phone_number: 0000, email_address: "email@contact.me")
+end
