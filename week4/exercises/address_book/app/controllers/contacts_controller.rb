7ce51df5158752ac0e50c472bfe0d27719a99b4d
Anais Engelmajer <anaiscorinne@gmail.com>
2016-02-01 18:47:31 -0500

    Add contact to database

--- week4/exercises/address_book/app/controllers/contacts_controller.rb
+++ week4/exercises/address_book/app/controllers/contacts_controller.rb
@@ -11,5 +11,18 @@ class ContactsController < ApplicationController
 	def create
     render(:text => "This route is contacts#create.")
   end
-  
+
+  def create
+    contact = Contact.new(
+      :name => params[:contact][:name],
+      :address => params[:contact][:address],
+      :phone_number => params[:contact][:phone_number],
+      :email_address => params[:contact][:email_address])
+
+    # Now we save the contact
+    contact.save
+
+    render(:text => contact.attributes)
+  end
+
 end
