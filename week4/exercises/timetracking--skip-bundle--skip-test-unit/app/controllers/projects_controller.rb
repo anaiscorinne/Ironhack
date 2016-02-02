20ac845f125c25d542f001c48654467b066a1482
Anais Engelmajer <anaiscorinne@gmail.com>
2016-02-02 11:09:09 -0500

    Understand relational models between classes

--- week4/exercises/timetracking--skip-bundle--skip-test-unit/app/controllers/projects_controller.rb
+++ week4/exercises/timetracking--skip-bundle--skip-test-unit/app/controllers/projects_controller.rb
@@ -1,8 +1,11 @@
 class ProjectsController < ApplicationController
   def index
-  	@projects = Project.last_created_projects(10)
-  	# puts @projects
-  	render 'index'
+  @projects = Project.last_created_projects(10)
+  	if @projects.empty?
+	  	render 'no_projects'
+  	else
+  		render 'index'
+  	end
   end
   def show
   	# id = params[:id]
