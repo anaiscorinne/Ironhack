094d769d2f76523462efe2828b05d0192e64d040
Anais Engelmajer <anaiscorinne@gmail.com>
2016-02-03 18:33:16 -0500

    Began configuration of products

--- week4/exercises/auction_site/app/models/product.rb
+++ week4/exercises/auction_site/app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ActiveRecord::Base
-	belongs_to :user
+	validates :title, presence: true
+	validates :description, presence: true
+	validates :deadline, presence: true
+
+	belongs_to :user, 
+	has_many :bids, dependent: :destroy
 end
