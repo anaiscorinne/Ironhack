85a352dfb1b8440e8bf79694f0adcd66d106044e
Anais Engelmajer <anaiscorinne@gmail.com>
2016-02-05 14:58:23 -0500

    Filter concerts based on dates and created an aside for future concerts

--- week4/exercises/music_concert_network/app/controllers/concerts_controller.rb
+++ week4/exercises/music_concert_network/app/controllers/concerts_controller.rb
@@ -1,5 +1,8 @@
 class ConcertsController < ApplicationController
   def index
+  	today = DateTime.now
+  	@concerts_this_month = Concert.where('date >= ? and date <= ?', today.beginning_of_month, today.end_of_month)
+  	@concerts_other_months = Concert.where('date > ?', today.end_of_month)
   end
 
   def new
