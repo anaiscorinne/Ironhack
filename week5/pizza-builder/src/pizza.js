13fbea5ec5dc5ca8c7055e1bcfb63f3eb46f88a6
Anais Engelmajer <anaiscorinne@gmail.com>
2016-02-08 15:54:05 -0500

    Set up pizza builder and added pepperoni amount together

---
+++ week5/pizza-builder/src/pizza.js
@@ -0,0 +1,42 @@
+// Write your Pizza Builder JavaScript in this file.
+$(".btn-pepperonni").on('click', function() {
+	$(".pep").toggle();
+	$(".btn-pepperonni").toggleClass("active");
+
+	if ($(".btn-pepperonni").hasClass("active") == false) {
+		var value = $("strong").text();
+		var valueWithoutDollarSign = value.slice(1)
+		var valueToInt = parseInt(valueWithoutDollarSign);
+		var newValue = valueToInt - 1;
+		var newValueToStr = newValue.toString();
+		$("strong").text("$" + newValueToStr);
+	}
+	else {
+		var value = $("strong").text();
+		var valueWithoutDollarSign = value.slice(1)
+		var valueToInt = parseInt(valueWithoutDollarSign);
+		var newValue = valueToInt + 1;
+		var newValueToStr = newValue.toString();
+		$("strong").text("$" + newValueToStr);
+	}
+})
+
+$(".btn-green-peppers").on('click', function() {
+	$(".green-pepper").toggle();
+	$(".btn-green-peppers").toggleClass("active");
+})
+
+$(".btn-mushrooms").on('click', function() {
+	$(".mushroom").toggle();
+	$(".btn-mushrooms").toggleClass("active");
+})
+
+$(".btn-crust").on("click", function() {
+	$(".crust").toggleClass("crust-gluten-free");
+	$(".btn-crust").toggleClass("active");
+})
+
+$(".btn-sauce").on("click", function() {
+	$(".sauce").toggleClass("sauce-white");
+	$(".btn-sauce").toggleClass("active");
+})
