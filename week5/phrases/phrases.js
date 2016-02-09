05da29e7cac1c9c0845058691f74f63fcd49d34d
Anais Engelmajer <anaiscorinne@gmail.com>
2016-02-09 09:27:57 -0500

    Randomize shown phrase

--- week5/phrases/phrases.js
+++ week5/phrases/phrases.js
@@ -2,5 +2,15 @@ var phrases = [
 	"May the Force be with you.",
 	"Today's not the day.",
 	"Say hello to my little friend!",
-	"Release the Kraken."
+	"Release the Kraken.",
+	"Don't answer a question with a question."
 ];
+
+var random0to4 = Math.random() * 4;
+var randomInt = Math.round(random0to4);
+var randomPhrase = phrases[randomInt];
+
+$(document).on("ready", function() 
+{
+	$('.js-phrase-heading').text(randomPhrase);
+});
