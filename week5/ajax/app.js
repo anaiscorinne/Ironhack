d4b141dd3c461430b874e934298378c4dccf9fec
Anais Engelmajer <anaiscorinne@gmail.com>
2016-02-09 11:36:38 -0500

    Add character button

--- week5/ajax/app.js
+++ week5/ajax/app.js
@@ -1,10 +1,49 @@
+$(document).on("ready", function () {
+	$(".js-show-characters").on("click", function () {
+		fetchCharacters();
+	});
+
+	$(".js-add-kylo").on("click", function () {
+		publishKylo();
+	});
+});
+
+function publishKylo () {
+	var newCharacter = {
+		name: "Emo Kylo Ren",
+		occupation: "Whiner",
+		weapon: "Complaining"
+	};
+	$.ajax({
+		type: "POST",
+		url: 'https://ironhack-characters.herokuapp.com/characters',
+		data: newCharacter,
+		success: function (response) {
+			var html = `
+			<li>
+				<h2>${newCharacter.name}</h2>
+				<p>${response.id}</p>
+				<p>${newCharacter.occupation}</p>
+				<p>${newCharacter.weapon}</p>
+			</li>
+		`;
+			$(".js-character-list").append(html);
+			alert("Emo Kylo Ren added succesfully!")
+		},
+		error: function () {
+			alert("Could not add Kylo Ren")
+		},
+	});
+}
+
+
+function fetchCharacters () {
 $.ajax({
 
 	url: 'https://ironhack-characters.herokuapp.com/characters',
 
 	success: function (response) {
 		console.log("good to go");
-		// console.log(response);
 		displayCharacters(response);
 
 	},
@@ -13,6 +52,7 @@ $.ajax({
 		console.log("Error");
 	}
 });
+}
 
 function displayCharacters (characters) {
 	characters.forEach( function (char) {
