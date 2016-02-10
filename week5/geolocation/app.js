37e5f763bcdca9d53ec2b66b7159b19b6a010116
Anais Engelmajer <anaiscorinne@gmail.com>
2016-02-10 13:53:05 -0500

    Use google maps API to show map location

--- week5/geolocation/app.js
+++ week5/geolocation/app.js
@@ -1,28 +1,45 @@
-if ("geolocation" in navigator) {
-	//Browser has geolocation
-
-	getUserLocation();
-}
-
-else {
-	// Browser does not have geolocation
-}
+$(document).on("ready", function () {
+	$('.where-am-i').on('click', function () {
+	if ("geolocation" in navigator) {
+		getUserLocation();
+	}
+	else {
+		alert("Geolocation is not available")
+	}
+	});
+});
 
 
 //-----------------Definitions Below----------------------
 
 function getUserLocation () {
+	console.log("Getting location...")
 	navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-	//optional third parameter: optionsObj
+	//optional third parameter: optionsObj - var above
 }
 
 function successCallback (position) {
-	console.log("Success");
-	console.log(position);
-	console.log("Lat:", position.coords.latitude);
-	console.log("Long:", position.coords.longitude);
+	var latitude = position.coords.latitude
+	var longitude = position.coords.longitude
+
+	displayMap(latitude, longitude);
+	// console.log("Lat:", position.coords.latitude);
+	// console.log("Long:", position.coords.longitude);
+	// $(".js-lat-value").text(position.coords.latitude);
+	// $(".js-lng-value").text(position.coords.longitude);
 }
 
 function errorCallback (error) {
-	console.log("Error:", error.message);
+	console.log("Getting location failed:", error.message);
+}
+
+function displayMap(lat, lng) {
+	var latitude = Number(lat);
+	var longitude = Number(lng);
+	var mapDiv = document.getElementById('map');
+        var map = new google.maps.Map(mapDiv, {
+          center: {lat: latitude, lng: longitude},
+          zoom: 8
+        });
+
 }
