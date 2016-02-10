dc289fc4b8aabe57f440cb5c92f1627fa2e0bf29
Anais Engelmajer <anaiscorinne@gmail.com>
2016-02-09 19:31:00 -0500

    Set up spotify api and search for artists

---
+++ week5/spotifyapi/app.js
@@ -0,0 +1,32 @@
+$(document).on("ready", function () {
+	$(".js-search-button").click(function(event) {
+		event.preventDefault();
+		searchArtist();
+	});
+});
+
+function searchArtist () {
+	var searchTerm = $('.js-search-term').val()
+	$.ajax({
+		url: 'https://api.spotify.com/v1/search?type=artist&query=' + searchTerm,
+		success: function (response) {
+			var artists = response.artists.items;
+			alert("Your search was successful")
+			displayResults(artists);
+		},
+		error: function () {
+			alert("Could not use search term.")
+		},
+	});
+}
+
+function displayResults (artists) {
+	artists.forEach( function (artist) {
+		var html = `
+		<li>
+			<h2>${artist.name}</h2>
+		</li>
+		`
+		$(".js-artist-list").append(html);
+	});
+}
