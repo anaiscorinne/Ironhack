f1ee2fcacadc6a7aae18bcc51273483e381a61aa
Anais Engelmajer <anaiscorinne@gmail.com>
2016-02-15 13:58:36 -0500

    Add SandwichIngredients model

--- w6/sandwichr/config/routes.rb
+++ w6/sandwichr/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :ingredients
+  
   resources :sandwiches, except: [:new, :edits]
+  resources :ingredients
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
