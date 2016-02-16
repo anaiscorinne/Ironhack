c67047d565fad59d2a814852b00941bd8e48d342
Anais Engelmajer <anaiscorinne@gmail.com>
2016-02-16 11:02:59 -0500

    Password authentication

--- w6/auth-exercise/app/controllers/users_controller.rb
+++ w6/auth-exercise/app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
 
   # renders the home page
-def home
-  @name = current_user ? @current_user.username : "Ironhacker"
-end
+  def home
+    @name = current_user ? @current_user.username : "Ironhacker"
+  end
 
   def index
     @users = User.all
@@ -29,7 +29,7 @@ end
   private
 
   def user_params
-     params.require(:user).permit(:username, :email)
+     params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 
 end
